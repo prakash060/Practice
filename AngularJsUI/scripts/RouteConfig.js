cb43bbc18f62ef082e6e5e4a2f99ef979a1d5990
Prakash <brprakash060@gmail.com>
2019-06-06 14:58:44 +0530

    Changed path

--- AngularJsUI/scripts/RouteConfig.js
+++ AngularJsUI/scripts/RouteConfig.js
@@ -4,13 +4,13 @@ app.config(function ($routeProvider, $locationProvider) {
     $routeProvider.caseInsensitiveMatch = true;
     $routeProvider
         .when("/home", {
-            templateUrl: "features/Home/Home.html",
+            templateUrl: "/features/Home/Home.html",
             controller: "HomeController"
         }).when("/courses", {
-            templateUrl: "features/Education/Course.html",
+            templateUrl: "/features/Education/Course.html",
             controller: "CourseController"
         }).when("/employees", {
-            templateUrl: "features/Employee/Employee.html",
+            templateUrl: "/features/Employee/Employee.html",
             controller: "EmployeeController"
         });
     $locationProvider.html5Mode({
