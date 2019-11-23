9346eba3e3c9f2faf0e39739f1fe8785b08027bf
Prakash Betageri <Brprakash060@gmail.com>
2019-11-24 00:18:15 +0530

    sdcfsdfdsf

--- Angular5UI/Properties/AssemblyInfo.cs
+++ Angular5UI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Angular5")]
+[assembly: AssemblyTitle("Angular5UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Angular5")]
+[assembly: AssemblyProduct("Angular5UI")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e558028d-809e-4f2b-b553-4debce2e148d")]
+[assembly: Guid("2a2ccd29-fe8e-401f-8c82-598d7048dfc1")]
 
 // Version information for an assembly consists of the following four values:
 //
