4ca16a2eea0eac806b8c930574df270edebb8c79
Prakash Betageri <Brprakash060@gmail.com>
2019-11-24 23:28:51 +0530

    sdadssdfsd

--- Angular5UI/hello-world/src/app/app-routing.module.ts
+++ Angular5UI/hello-world/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {EmployeeListComponent} from './employee/employee-list.component';
 import {EmployeeCreateComponent} from './employee/employee-create.component'
+import {ImagegalleryComponent} from './imagegallery/imagegallery.component'
 
 
 const routes: Routes = [
   { path:'list', component:EmployeeListComponent },
   { path:'create', component:EmployeeCreateComponent },
+  { path:'img', component:ImagegalleryComponent },
   { path:'', redirectTo:'/list', pathMatch :'full' }
 ];
 
--- Angular5UI/hello-world/src/app/app.module.ts
+++ Angular5UI/hello-world/src/app/app.module.ts
@@ -4,19 +4,24 @@ import {ReactiveFormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { EmployeeListComponent } from './employee/employee-list.component';
 import { EmployeeCreateComponent } from './employee/employee-create.component';
+import { ImagegalleryComponent } from './imagegallery/imagegallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
     EmployeeCreateComponent,
+    ImagegalleryComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
