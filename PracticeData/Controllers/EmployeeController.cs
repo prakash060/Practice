d2aab3058314941b211ac27d7626cd1aeb17ba1e
prakash060 <brprakash060@gmail.com>
2020-08-17 20:33:55 +0530

    Added few files

--- PracticeData/Controllers/EmployeeController.cs
+++ PracticeData/Controllers/EmployeeController.cs
@@ -18,10 +18,10 @@ namespace PracticeData.Controllers
             var employees = new List<Employee>
             {
                 new Employee{Id=1, Name="Prakash",City="Naganur"},
-                new Employee{Id=1, Name="Shweta",City="Naganur"},
+                new Employee{Id=1, Name="Nic",City="Naganur"},
                 new Employee{Id=1, Name="Manu",City="Naganur"},
-                new Employee{Id=1, Name="Ramesh",City="Naganur"},
-                new Employee{Id=1, Name="Ratnavva",City="Naganur"}
+                new Employee{Id=1, Name="Clerk",City="Naganur"},
+                new Employee{Id=1, Name="John",City="Naganur"}
             };
             return Ok(employees);
         }
