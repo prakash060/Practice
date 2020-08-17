2439649600bbc58596578f5a2908e1ad7a55a981
prakash060 <brprakash060@gmail.com>
2020-08-17 14:19:08 +0530

    Added swagger to Common API

--- React/react-shop/src/components/HomeComponent.jsx
+++ React/react-shop/src/components/HomeComponent.jsx
@@ -2,34 +2,71 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default class HomeComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      email: "",
+      dob: "",
+    };
+  }
+
+  inputChangeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    console.log(this.state);
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <div class="container h-100">
-        <div class="row h-100 justify-content-center align-items-center">
-          <div class="col-10 col-md-8 col-lg-4">
-            <form>
+      <div className="container h-100">
+        <div className="row h-100 justify-content-center align-items-center">
+          <div className="col-10 col-md-6 col-lg-4">
+            <form onSubmit={this.handleSubmit}>
               <h1>Welcome Form !</h1>
-              <div class="form-group">
-                <label for="username">User Name:</label>
+              <div className="form-group">
+                <label htmlFor="userName">User Name:</label>
                 <input
                   type="text"
-                  class="form-control username"
-                  id="username"
+                  className="form-control"
+                  id="userName"
                   placeholder="Enter User Name"
-                  name="username"
+                  name="userName"
+                  value={this.state.userName}
+                  onChange={this.inputChangeHandler}
                 />
               </div>
-              <div class="form-group">
-                <label for="email">Email:</label>
+              <div className="form-group">
+                <label htmlFor="email">Email:</label>
                 <input
                   type="text"
-                  class="form-control email"
+                  className="form-control"
                   id="email"
                   placeholder="Enter Email"
                   name="email"
+                  value={this.state.email}
+                  onChange={this.inputChangeHandler}
                 />
               </div>
-              <button type="submit" class="btn btn-secondary btn-customized">
+              <div className="form-group">
+                <label htmlFor="dob">Date Of Birth:</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  id="dob"
+                  placeholder="Enter date of birth"
+                  name="dob"
+                  value={this.state.dob}
+                  onChange={this.inputChangeHandler}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-secondary btn-customized"
+              >
                 Submit
               </button>
             </form>
