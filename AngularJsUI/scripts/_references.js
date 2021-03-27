0ca490ccb6f4745281b82fbcaa00118aeca65bff
prakash060 <brprakash060@gmail.com>
2021-03-27 13:37:17 +0530

    Updated packages

--- AngularJsUI/scripts/_references.js
+++ AngularJsUI/scripts/_references.js
@@ -1,4 +1,5 @@
-﻿/// <autosync enabled="false" />
+﻿/// <reference path="jquery-3.6.0.js" />
+/// <autosync enabled="false" />
 /// <reference path="angular.js" />
 /// <reference path="angular-animate.js" />
 /// <reference path="angular-route.js" />
