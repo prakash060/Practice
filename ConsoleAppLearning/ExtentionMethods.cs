767cc83f70f199a3ef5c58eb74b18667b84cceae
prakash060 <43981043+prakash060@users.noreply.github.com>
2023-12-10 11:59:35 +0530

    Fixed bugs

--- ConsoleAppLearning/ExtentionMethods.cs
+++ ConsoleAppLearning/ExtentionMethods.cs
@@ -1,4 +1,4 @@
-﻿using PracticeData;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +9,9 @@ namespace ConsoleAppLearning
 {     
     public static class ExtentionMethods
     {
-        public static string ToEmpString(this Employee employee)
-        {
-            return $"Name : {employee.Name}, Id : {employee.Id}, City : {employee.City}";
-        }
+        //public static string ToEmpString(this Employee employee)
+        //{
+        //    return $"Name : {employee.Name}, Id : {employee.Id}, City : {employee.City}";
+        //}
     }
 }
